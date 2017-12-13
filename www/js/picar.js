2d00a2618ae87b84417eb149caafd5c8e4ac7360
Sam Jackson <sjackson@drakeent.com>
2017-12-13 13:47:28 -0500

    Node-Red

---
+++ www/js/picar.js
@@ -0,0 +1,20 @@
+$('document').ready( function (){
+    $('#copy-picar-auth-flow-code').on('click', function(){
+        copyToClipboard('.auth-code');
+    });
+    $('#copy-picar-control-flow-code').on('click', function(){
+        copyToClipboard('.control-code');
+    });
+});
+
+// A PEN BY Shaik Maqsood
+// https://codepen.io/shaikmaqsood/pen/XmydxJ/
+var copyToClipboard = function(element){
+    var $temp = $("<input>");
+    $("body").append($temp);
+    //      Minify
+    $temp.val(JSON.stringify($.parseJSON($(element).text()))).select();
+    //$temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
