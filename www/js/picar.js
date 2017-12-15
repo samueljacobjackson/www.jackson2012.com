739babed93e678036947029fafa4a6dfc34b7178
Sam Jackson <sjackson@drakeent.com>
2017-12-15 14:28:52 -0500

    ReWork

--- www/js/picar.js
+++ www/js/picar.js
@@ -1,15 +1,15 @@
-$('document').ready( function (){
-    $('#copy-picar-auth-flow-code').on('click', function(){
+$('document').ready(function() {
+    $('#copy-picar-auth-flow-code').on('click', function() {
         copyToClipboard('.auth-code');
     });
-    $('#copy-picar-control-flow-code').on('click', function(){
+    $('#copy-picar-control-flow-code').on('click', function() {
         copyToClipboard('.control-code');
     });
 });
 
 // A PEN BY Shaik Maqsood
 // https://codepen.io/shaikmaqsood/pen/XmydxJ/
-var copyToClipboard = function(element){
+var copyToClipboard = function(element) {
     var $temp = $("<input>");
     $("body").append($temp);
     //      Minify
