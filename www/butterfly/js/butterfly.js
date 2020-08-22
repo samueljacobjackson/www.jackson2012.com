501591c39f696b0fe5ad143065ad7f4f2f7f3098
Sam Jackson <sam@jackson2012.com>
2020-08-22 10:28:08 -0400

    Butterflies

---
+++ www/butterfly/js/butterfly.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $('.pics').on('click', (e) => {
+        $('.lightbox-image').empty()
+        var url = $(e.currentTarget).find('img').attr('src');
+        $('.lightbox-image').html('<img src="' + url.replace('image', 'large/image') + '">');
+        $('.lightbox').addClass('d-block');
+        $('.lightbox').removeClass('d-none');
+    });
+
+    $('.lightbox').on('click', (e) => {
+        $('.lightbox').addClass('d-none');
+        $('.lightbox').removeClass('d-block');
+    });
+});
--- www/js/jackson2012.js
+++ www/js/jackson2012.js
@@ -26,11 +26,100 @@
   var navbarCollapse = function() {
       if ($("#mainNav").offset().top > 100) {
           $("#mainNav").addClass("navbar-shrink");
+          $('.btn-top').css('display','block');
       } else {
           $("#mainNav").removeClass("navbar-shrink");
+          $('.btn-top').css('display','none');
       }
   };
   navbarCollapse();
   $(window).scroll(navbarCollapse);
 
 })(jQuery);
+
+let modalId = $('#gallery');
+
+$(document)
+  .ready(function () {
+
+    loadGallery(true, 'a.thumbnail');
+
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+      $('#previous-print, #next-print')
+        .show();
+      if (counter_max === counter_current) {
+        $('#next-print')
+          .hide();
+      } else if (counter_current === 1) {
+        $('#previous-print')
+          .hide();
+      }
+    }
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#next-print, #previous-print')
+        .click(function () {
+          if ($(this)
+            .attr('id') === 'previous-print') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#gallery-title')
+          .text($sel.data('title'));
+        $('#gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
+    }
+  });
+
+// build key actions
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37: // left
+        if ((modalId.data('bs.modal') || {})._isShown && $('#previous-print').is(":visible")) {
+          $('#previous-print')
+            .click();
+        }
+        break;
+
+      case 39: // right
+        if ((modalId.data('bs.modal') || {})._isShown && $('#next-print').is(":visible")) {
+          $('#next-print')
+            .click();
+        }
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
