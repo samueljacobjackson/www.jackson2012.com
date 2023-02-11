404fe8254dede954502dae5c42ee0bad0bcbb6ce
Sam Jackson <sam@jackson2012.com>
2023-02-11 08:18:39 -0500

    hhh

--- stable-diffusion/js/preload.js
+++ stable-diffusion/js/preload.js
@@ -1,3 +1,7 @@
-for(var i = 1; i <= 150; i++){
-    var $div = $(img)
+for(var i = 1; i <= 172; i++){
+    var n = String(i).padStart(5, '0');
+    var $div = $("<div>", { "class": "mb-3 pics animation" });
+    var $img = $("<img>", { "class": "img-fluid hide", "src": 'img/image_' + n + '.png', "alt": "image_" + n});
+    $div.append($img);
+    $('#gallery').append($div);
 }
